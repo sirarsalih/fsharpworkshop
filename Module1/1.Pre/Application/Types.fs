b31593d1346e6160454a8a0112e277d0a1959537
sirar87 <sirars@gmail.com>
2015-03-17 19:37:13 +0100

    Completed Module 1 Pre.

--- Module1/1.Pre/Application/Types.fs
+++ Module1/1.Pre/Application/Types.fs
@@ -1,2 +1,14 @@
 ﻿module Types
 
+type Customer =
+    {
+    Id: int
+    IsVip: bool
+    Credit: float };;
+
+let customer1 = {Id = 1; IsVip = false; Credit = 10.0};;
+
+let customer2 = {Id = 2; IsVip = false; Credit = 0.0};;
+
+
+
